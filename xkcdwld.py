c2979c74f50009d754ddb134ec8bf5db5a82c12e
Ashish <ashish1shenoy@gmail.com>
2019-06-14 19:49:34 +0530

    tidied up code

--- xkcdwld.py
+++ xkcdwld.py
@@ -1,5 +1,10 @@
-import requests, bs4, re, sys, os
+import requests
 import logging
+import re
+import os
+import sys
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 
 # Logging control.
@@ -13,21 +18,20 @@ logging.info("Start of execution")
 
 
 # Open the xkcd page and create a response object.
-html = requests.get("http://www.xkcd.com", timeout = (3.05, 27))
-if html.status_code != 200:
+html = requests.get("https://www.xkcd.com", timeout = (3.05, 27))
+if not (300 >= html.status_code >= 200):
+    logging.critical("Unexpected error during initial connection.")
     sys.exit("An unexpected error has occurred with the connection to the server.")
-xkcdSoup = bs4.BeautifulSoup(html.text, features = "html.parser")
+xkcdSoup = BeautifulSoup(html.text, features = "html.parser")
 
 
 # Counting the number of xkcd comics made till date.
-countObject = xkcdSoup.select("#middleContainer", parser = "html.parser")
-text = countObject[0].getText()
-countText = [
-    line
-    for line in text.split("\n")
-    if line.startswith("Permanent link to this comic: ")
-][0]
-logging.debug(f"Text to extract NUMBER_OF_COMICS from: '{countText}'")
+countObject = xkcdSoup.select_one("#middleContainer", parser = "html.parser")
+text = countObject.getText()
+for line in text.split("\n"):
+    if line.startswith("Permanent link to this comic: "):
+        countText = line
+logging.debug(f"Text to extract total number of comics made from: '{countText}'")
 numRegex = re.compile(r"https://xkcd\.com/(\d+)/")
 matchObject = numRegex.search(countText)
 numOfComics = int(matchObject.group(1))
@@ -43,27 +47,24 @@ else:
 logging.info(f"{NUM_TO_DOWNLOAD} comics to be downloaded.")
 
 
-# Create image directory.
-if not os.path.exists("XKCD Comics"):
-    os.mkdir("XKCD Comics")
-
-
+# TODO: Add a progress bar.
 # Select each image , write it to disk and open the previous image.
+os.makedirs("XKCD Comics", exist_ok = True)
 for num in range(1, NUM_TO_DOWNLOAD + 1):
     comicNum = numOfComics - (num - 1)
 
     # Downloading the image.
     downloadError = False
-    imageElem = xkcdSoup.select("#comic img", parser = "html.parser")
+    imageElem = xkcdSoup.select_one("#comic img", parser = "html.parser")
     if imageElem != []:
-        imageLink = imageElem[0].get("src") 
+        imageLink = imageElem.get("src") 
     else:
         print(f"Could not find comic image {comicNum}")
         logging.debug(f"Could not find comic image {comicNum}")
         downloadError = True
     print(f"Downloading image {num}, comic number {comicNum}...")
     try:
-        imageObject = requests.get("http:" + imageLink, timeout = (3.05, 27))
+        imageObject = requests.get("https:" + imageLink, timeout = (3.05, 27))
     except Exception as e:
         print(f"There was an error downloading comic {comicNum}")
         logging.debug(f"Error downloading comic {comicNum}")
@@ -76,18 +77,18 @@ for num in range(1, NUM_TO_DOWNLOAD + 1):
                 image.write(chunk)
 
     # Updating xkcdSoup to contain the previous comic webpage.
-    prevTag = xkcdSoup.select('a[rel="prev"]', parser = "html.parser")[0]
+    prevTag = xkcdSoup.select_one('a[rel="prev"]', parser = "html.parser")
     subLink = prevTag.get("href")
     if subLink == "#":
         logging.info("All images have been downloaded.")
         break
-    prevImageLink = "http://www.xkcd.com" + subLink
+    prevImageLink = "https://www.xkcd.com" + subLink
     html = requests.get(prevImageLink, timeout = (3.05, 27))
-    if html.status_code != 200:
+    if not (300 >= html.status_code >= 200):
         print(f"{num} images have been successfully downloaded.")
         logging.critical(f"Unexpected error occurred while downloading comic image {comicNum}")
         sys.exit("An unexpected error has occurred with the connection to the server.")
-    xkcdSoup = bs4.BeautifulSoup(html.text, features = "html.parser")
+    xkcdSoup = BeautifulSoup(html.text, features = "html.parser")
 
 
 # Success Message.
