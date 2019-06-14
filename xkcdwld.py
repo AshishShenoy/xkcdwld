edc48f0f01b9c6d299363388f6f7fb463fe19a51
Ashish <ashish1shenoy@gmail.com>
2019-06-14 22:52:33 +0530

    code refactored into functions

--- xkcdwld.py
+++ xkcdwld.py
@@ -7,90 +7,124 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-# Logging control.
-logging.basicConfig(
-    filename = "xkcdwld.log",
-    level = logging.DEBUG,
-    format = " %(asctime)s - %(levelname)s - %(message)s",
-)
-logging.disable(logging.CRITICAL)
-logging.info("Start of execution")
+def initLogging():
+    logging.basicConfig(
+        filename = "xkcdwld.log",
+        level = logging.DEBUG,
+        format = " %(asctime)s - %(levelname)s - %(message)s",
+    )
+    # logging.disable(logging.CRITICAL)
+    logging.info("Start of execution")
 
 
-# Open the xkcd page and create a response object.
-html = requests.get("https://www.xkcd.com", timeout = (3.05, 27))
-if not (300 >= html.status_code >= 200):
-    logging.critical("Unexpected error during initial connection.")
-    sys.exit("An unexpected error has occurred with the connection to the server.")
-xkcdSoup = BeautifulSoup(html.text, features = "html.parser")
+def getInitialSoup():
+    html = requests.get("https://www.xkcd.com", timeout = (3.05, 27))
+    if not (200 <= html.status_code <= 300):
+        logging.critical("Unexpected error during initial connection.")
+        sys.exit("An unexpected error has occurred with the connection to the server.")
+    return BeautifulSoup(html.text, features = "html.parser")
 
 
-# Counting the number of xkcd comics made till date.
-countObject = xkcdSoup.select_one("#middleContainer", parser = "html.parser")
-text = countObject.getText()
-for line in text.split("\n"):
-    if line.startswith("Permanent link to this comic: "):
-        countText = line
-logging.debug(f"Text to extract total number of comics made from: '{countText}'")
-numRegex = re.compile(r"https://xkcd\.com/(\d+)/")
-matchObject = numRegex.search(countText)
-numOfComics = int(matchObject.group(1))
-logging.debug(f"Number of comics = {numOfComics}")
-
-
-# Allowing the option of downloading a specific number of latest comics through CL arguments.
-# Give no CL arguments to download all comics.
-if len(sys.argv) > 1:
-    NUM_TO_DOWNLOAD = int(sys.argv[1])
-else:
-    NUM_TO_DOWNLOAD = numOfComics
-logging.info(f"{NUM_TO_DOWNLOAD} comics to be downloaded.")
+def getTotalComics(homepageSoup):
+    countObject = homepageSoup.select_one("#middleContainer", parser = "html.parser")
+    text = countObject.getText()
+    for line in text.split("\n"):
+        if line.startswith("Permanent link to this comic: "):
+            countText = line
+    logging.debug(f"Text to extract total number of comics made from: '{countText}'")
+    numRegex = re.compile(r"https://xkcd\.com/(\d+)/")
+    matchObject = numRegex.search(countText)
+    total = int(matchObject.group(1))
+    logging.debug(f"Number of comics = {total}")
+    return total
 
 
 # TODO: Add a progress bar.
-# Select each image , write it to disk and open the previous image.
-os.makedirs("XKCD Comics", exist_ok = True)
-for num in range(1, NUM_TO_DOWNLOAD + 1):
-    comicNum = numOfComics - (num - 1)
+# TODO: Add multithreaded downloads.
+# TODO: Add GUI.
+def downloadComics(numToDownload, total, soup):
+    os.makedirs("XKCD Comics", exist_ok = True)
+    logging.info(f"{numToDownload} comics to be downloaded.")
 
-    # Downloading the image.
-    downloadError = False
-    imageElem = xkcdSoup.select_one("#comic img", parser = "html.parser")
-    if imageElem != []:
-        imageLink = imageElem.get("src") 
+    for num in range(1, numToDownload + 1):
+        comicNo = total - (num - 1)
+
+        imageObject, downloadError = downloadComic(comicNo, soup)
+        if not downloadError:
+            saveImage(comicNo, imageObject)
+        
+        prevSoup, linkError = pointToPreviousSoup(comicNo, soup)
+        if linkError:
+            print(f"{num} images have been successfully downloaded.")
+            logging.critical(f"Unexpected error occurred while downloading comic image {comicNo}")
+            sys.exit("An unexpected error has occurred with the connection to the server.")
+        soup = prevSoup
+
+
+def downloadComic(comicNo, soup):
+    error = False
+    imageElem = soup.select_one("#comic img", parser = "html.parser")
+    if imageElem == []:
+        print(f"Could not find comic image {comicNo}.")
+        logging.debug(f"Could not find comic image {comicNo}.")
+        error = True 
     else:
-        print(f"Could not find comic image {comicNum}")
-        logging.debug(f"Could not find comic image {comicNum}")
-        downloadError = True
-    print(f"Downloading image {num}, comic number {comicNum}...")
+        imageLink = imageElem.get("src")
+    
+    print(f"Downloading comic number {comicNo}...")
     try:
         imageObject = requests.get("https:" + imageLink, timeout = (3.05, 27))
     except Exception as e:
-        print(f"There was an error downloading comic {comicNum}")
-        logging.debug(f"Error downloading comic {comicNum}")
-        downloadError = True
+        print(f"There was an error downloading comic {comicNo}.")
+        logging.debug(f"Error downloading comic {comicNo}: {e}")
+        error = True
 
-    # Writing the image to disk and numbering it.
-    if not downloadError:
-        with open(os.path.join("XKCD Comics", "xkcd" + str(comicNum) + ".png"), "wb") as image:
-            for chunk in imageObject.iter_content(1024 * 128):
-                image.write(chunk)
+    return imageObject, error
 
-    # Updating xkcdSoup to contain the previous comic webpage.
-    prevTag = xkcdSoup.select_one('a[rel="prev"]', parser = "html.parser")
+
+def saveImage(comicNo, image):
+    with open(os.path.join("XKCD Comics", "xkcd" + str(comicNo) + ".png"), "wb") as imageFile:
+        for chunk in image.iter_content(1024 * 128):
+            imageFile.write(chunk)
+
+
+def pointToPreviousSoup(comicNo, soup):
+    error = False
+    prevTag = soup.select_one('a[rel="prev"]', parser = "html.parser")
     subLink = prevTag.get("href")
-    if subLink == "#":
-        logging.info("All images have been downloaded.")
-        break
     prevImageLink = "https://www.xkcd.com" + subLink
     html = requests.get(prevImageLink, timeout = (3.05, 27))
-    if not (300 >= html.status_code >= 200):
-        print(f"{num} images have been successfully downloaded.")
-        logging.critical(f"Unexpected error occurred while downloading comic image {comicNum}")
-        sys.exit("An unexpected error has occurred with the connection to the server.")
-    xkcdSoup = BeautifulSoup(html.text, features = "html.parser")
+    if not (200 <= html.status_code <= 300):
+        error = True
+        return None, error
+
+    newSoup = BeautifulSoup(html.text, features = "html.parser")
+
+    return newSoup, error
 
 
-# Success Message.
-print(f"{num} images have been successfully downloaded.")
-logging.info(f"{num} images have been successfully downloaded.")
+# TODO: Display a pop-up.
+def displaySuccessMessage(num):
+    print(f"{num} images have been successfully downloaded.")
+    logging.info(f"{num} images have been successfully downloaded.")
+
+
+def main():
+    initLogging()
+
+    xkcdSoup = getInitialSoup()
+    totalComics = getTotalComics(xkcdSoup)
+
+    # Allowing the option of downloading a specific number of latest comics through CL arguments.
+    # Give no CL arguments to download all comics.
+    if len(sys.argv) > 1:
+        numToDownload = int(sys.argv[1]) if int(sys.argv[1]) <= totalComics else totalComics
+    else:
+        numToDownload = totalComics
+
+    downloadComics(numToDownload, totalComics, xkcdSoup)
+    displaySuccessMessage(numToDownload)
+
+
+if __name__ == "__main__":
+    main()
